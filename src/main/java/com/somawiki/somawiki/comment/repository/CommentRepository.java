263ff20263accdfc0318b558d2d1aa5eda49792c
TaegyunWoo <dnxprbs@gmail.com>
2022-04-27 12:50:34 +0900

    [FIX] CommentRepository Pageable 오류 해결
    - 잘못된 라이브러리를 import 하여, bean 생성에 실패함.
    - 따라서 수정함.

--- src/main/java/com/somawiki/somawiki/comment/repository/CommentRepository.java
+++ src/main/java/com/somawiki/somawiki/comment/repository/CommentRepository.java
@@ -4,9 +4,8 @@ import com.somawiki.somawiki.comment.domain.Comment;
 import com.somawiki.somawiki.review.domain.Review;
 import com.somawiki.somawiki.user.domain.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.print.Pageable;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAll(Pageable pageable);
