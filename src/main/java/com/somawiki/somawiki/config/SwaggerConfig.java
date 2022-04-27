2d528acd6e7d68476836db914c28d36e0f86f2a9
TaegyunWoo <dnxprbs@gmail.com>
2022-04-27 14:37:44 +0900

    [FEAT] 스웨거 추가

---
+++ src/main/java/com/somawiki/somawiki/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.somawiki.somawiki.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI api() {
+    Info info = new Info()
+      .title("Soma-Wiki")
+      .version("V1.0")
+      .license(new License()
+        .name("Apache License Version 2.0")
+        .url("http://www.apache.org/license/LICENSE-2.0"));
+
+    SecurityScheme auth = new SecurityScheme()
+      .type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.COOKIE).name("JSESSIONID");
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList("basicAuth");
+
+    return new OpenAPI()
+      .components(new Components().addSecuritySchemes("basicAuth", auth))
+      .addSecurityItem(securityRequirement)
+      .info(info);
+  }
+
+}
