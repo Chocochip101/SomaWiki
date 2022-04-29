480db22685b93dbe8769644423bfd568e031c150
TaegyunWoo <dnxprbs@gmail.com>
2022-04-30 04:05:23 +0900

    [FIX] Add MentorDetail swagger summary

--- src/main/java/com/somawiki/somawiki/mentor/controller/MentorController.java
+++ src/main/java/com/somawiki/somawiki/mentor/controller/MentorController.java
@@ -3,6 +3,7 @@ package com.somawiki.somawiki.mentor.controller;
 import com.somawiki.somawiki.mentor.dto.MentorDetailResponseDto;
 import com.somawiki.somawiki.mentor.exception.WrongMentorException;
 import com.somawiki.somawiki.mentor.service.MentorService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MentorController {
   private final MentorService mentorService;
 
+  @Operation(summary = "멘토 디테일")
   @GetMapping("/{mentorIdx}")
   public MentorDetailResponseDto detailsOfMentor(@PathVariable long mentorIdx) throws WrongMentorException {
     MentorDetailResponseDto result = mentorService.showDetailsOfMentor(mentorIdx);
