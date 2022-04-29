a6fa17f59d76ff6cf230e31959c7e284a0e7bbb6
TaegyunWoo <dnxprbs@gmail.com>
2022-04-30 03:30:34 +0900

    [FEAT] Add NoArgsConstruction to DTO

--- src/main/java/com/somawiki/somawiki/user/dto/LoginRequestDto.java
+++ src/main/java/com/somawiki/somawiki/user/dto/LoginRequestDto.java
@@ -2,12 +2,14 @@ package com.somawiki.somawiki.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
   @NotEmpty
