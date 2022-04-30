4049409b679e00ce98568aef92da372293c30ad8
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2022-04-30 15:10:25 +0900

    feat: swagger 설정 추가

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Soma-Wiki-backend
-
 soma-wiki web backend server
 
-## Swagger URL
-http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#
+## API 명세서
+https://soma-wiki.kro.kr/index.html
--- src/main/java/com/somawiki/somawiki/config/SwaggerConfig.java
+++ src/main/java/com/somawiki/somawiki/config/SwaggerConfig.java
@@ -6,11 +6,16 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+  @Value("${swagger-docs.url}")
+  private String url;
 
   @Bean
   public OpenAPI api() {
@@ -27,8 +32,8 @@ public class SwaggerConfig {
 
     return new OpenAPI()
       .components(new Components().addSecuritySchemes("basicAuth", auth))
+      .addServersItem(new Server().url(url))
       .addSecurityItem(securityRequirement)
       .info(info);
   }
-
 }
