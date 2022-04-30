6e77de94106890e067fead986bd6cc7dec9d5375
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2022-04-30 13:09:06 +0900

    feat(LoginCheckInterceptor): GET 요청에 대해 세션 확인 폐지

--- src/main/java/com/somawiki/somawiki/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/somawiki/somawiki/interceptor/LoginCheckInterceptor.java
@@ -12,8 +12,12 @@ import java.util.Objects;
 public class LoginCheckInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    String method = request.getMethod();
+    if ("GET".equalsIgnoreCase(method)) {
+      return true;
+    }
     HttpSession session = request.getSession(false);
-    if (session == null || Objects.isNull(session.getAttribute("loginUser"))) {
+    if (Objects.isNull(session) || Objects.isNull(session.getAttribute("loginUser"))) {
       response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
       return false;
     }
